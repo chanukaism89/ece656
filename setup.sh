70edc076cf1c6b5b1a145cd4d7fe821a632ae2ee
chanukaism89 <60856639+chanukaism89@users.noreply.github.com>
2020-12-23 11:49:42 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install python3-dev libssl-dev python3
+sudo apt-get install default-libmysqlclient-dev build-essential
 
 # install pip
 apt-get install python3-venv
